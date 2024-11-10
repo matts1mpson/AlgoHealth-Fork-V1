426bf97feafc82e9fa7189cdfdf612daa1732cb7
DanielXie1795 <danielxie1795@gmail.com>
2024-11-10 18:43:19 -0500

    made prototypes for login and a temporary data access object

---
+++ src/main/java/data_access/TempDAOInMemory.java
@@ -0,0 +1,55 @@
+package data_access;
+
+import data.AccountInfo;
+import data.DayInfo;
+import data.Food;
+import use_case.food_logging.LogFoodDataAccessInterface;
+import use_case.login.LoginDataAccessInterface;
+import use_case.logout.LogoutDataAccessInterface;
+import use_case.signup.SignupDataAccessInterface;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TempDAOInMemory implements LoginDataAccessInterface, LogoutDataAccessInterface, SignupDataAccessInterface,
+        LogFoodDataAccessInterface {
+
+    private final Map<String, AccountInfo> accounts = new HashMap<>();
+    private String currentUsername;
+
+    public void save(Food food) {
+        List<DayInfo> currentDays = accounts.get(currentUsername).getDays();
+        int currentDayIndex = currentDays.size() - 1;
+        currentDays.get(currentDayIndex).addToFoodLog(food);
+        accounts.get(currentUsername).setDays(currentDays);
+    }
+
+    public void save(AccountInfo accountInfo) {
+
+    }
+
+    public AccountInfo get(String username) {
+        return new AccountInfo(LocalDate.now(), 0, 0,
+                "", "", "", "", new ArrayList<>());
+    }
+
+    public void put(String username, AccountInfo accountInfo) {
+
+    }
+
+    public boolean existsByName(String username) {
+        return true;
+    }
+
+    public String getCurrentUsername() {
+        return currentUsername;
+    }
+
+    public void setCurrentUsername(String username) {
+        this.currentUsername = username;
+    }
+
+}
---
+++ src/main/java/view/LoginView/LoginView.java
@@ -0,0 +1,29 @@
+package view.LoginView;
+
+import javax.swing.*;
+
+public class LoginView {
+
+    private final LoginViewBuilder loginViewBuilder;
+
+    public LoginView(LoginViewBuilder loginViewBuilder) {
+        this.loginViewBuilder = loginViewBuilder;
+    }
+
+    public void generateLoginView() {
+        JFrame loginView = loginViewBuilder.addWelcomeLabel().
+                addUsernamePanel().
+                addPasswordPanel().
+                addButtonPanel().
+                build();
+
+        loginView.pack();
+        loginView.setVisible(true);
+    }
+
+    public static void main(String[] args) {    // test for the gui
+        LoginView loginView = new LoginView(new LoginViewBuilder(new JPanel()));
+
+        loginView.generateLoginView();
+    }
+}
